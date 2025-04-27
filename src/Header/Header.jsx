92f460eb3a0cf3d2c0717a5a05f211dd77beb11e
Dineshsethu001 <dineshsethu15981@gmail.com>
2025-04-27 19:00:53 +0530

    files updated

---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "../assets/img/Chef Claude Icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
